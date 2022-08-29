c8370fb20ac5e71dcd0b0bc2e5590bcf67343f2e
la7ci9 <akarmiis997@gmail.com>
2022-08-29 15:41:26 +0200

    solve

---
+++ NumOfTrailZero.java
@@ -0,0 +1,27 @@
+/*
+Write a program that will calculate the number of trailing zeros in a factorial of a given number.
+
+N! = 1 * 2 * 3 *  ... * N
+
+Be careful 1000! has 2568 digits...
+
+For more info, see: http://mathworld.wolfram.com/Factorial.html
+
+Examples
+zeros(6) = 1
+# 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
+
+zeros(12) = 2
+# 12! = 479001600 --> 2 trailing zeros
+*/
+public class NumOfTrailZero {
+    public static int zeros(int n) {
+        // your beatiful code here
+      int count = 0;
+      
+      for (int i = 5; n / i >= 1; i *= 5)
+          count += n / i;
+   
+      return count;
+    }
+  }
